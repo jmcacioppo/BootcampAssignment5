8138210abfdc86a9175f087c803e00688de45314
jmcacioppo <jmcacioppo0531@gmail.com>
2017-09-15 13:24:21 -0400

    heroku

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+
+module.exports = {
+  db: {
+    uri: 'mongodb://jmcacioppo:bootcamp4@ds129344.mlab.com:29344/bootcamp4', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: 'AIzaSyBirIOtBQayM3OdXm9KIIxyy_PeG5ET8G8'
+  },
+  port: 8080
+};
